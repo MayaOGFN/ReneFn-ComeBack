ca594e6d22536f5c90011054332810218b3f151b
MayaOGFN <evieenjoy800@gmail.com>
2025-12-22 17:42:26 +1100

    Update server.js

--- server.js
+++ server.js
@@ -1,54 +1,46 @@
 const express = require('express');
 const cors = require('cors');
+const fs = require('fs');
+const path = require('path');
 const app = express();
 
-// Render uses dynamic ports; this line is critical for deployment
 const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
 
-// --- 1. VERSION API ---
-// If the launcher's CurrentVersion (1.0) is LOWER than this number, 
-// the launcher will block access and show "UPDATE REQUIRED".
+// Helper function to read JSON files easily
+const readJson = (fileName) => {
+    const filePath = path.join(__dirname, 'data', fileName);
+    return JSON.parse(fs.readFileSync(filePath, 'utf8'));
+};
+
+// --- ROUTES ---
+
+// 0. Home Page (Fixes "Cannot GET /")
+app.get('/', (req, res) => {
+    res.send("<h1>ReneFN Backend is Online</h1><p>The API is running correctly.</p>");
+});
+
+// 1. Version API
 app.get('/launcher/version', (req, res) => {
-    res.send("1.0"); 
+    const data = readJson('launcherVersion.json');
+    res.send(data.version);
 });
 
-// --- 2. NEWS API ---
-// These appear in the top box of your news/shop page
+// 2. News API
 app.get('/news', (req, res) => {
-    res.json([
-        {
-            "name": "Welcome to ReneFN",
-            "image": "https://i.imgur.com/DYhYsgd.png"
-        },
-        {
-            "name": "Join our Discord",
-            "image": "https://i.imgur.com/your-discord-image.png"
-        }
-    ]);
+    res.json(readJson('news.json'));
 });
 
-// --- 3. ITEM SHOP API ---
-// These appear in the bottom box of your news/shop page
+// 3. Item Shop API
 app.get('/shop', (req, res) => {
-    res.json([
-        {
-            "name": "Renegade Raider",
-            "image": "https://fortnite-api.com/images/cosmetics/br/cid_028_athena_commando_f/featured.png"
-        },
-        {
-            "name": "Ariel Specialist",
-            "image": "https://fortnite-api.com/images/cosmetics/br/cid_017_athena_commando_f/featured.png"
-        }
-    ]);
+    res.json(readJson('shop.json'));
 });
 
-// --- 4. REDEEM API ---
-// This is the page that opens when the 'Redeem' button is clicked
+// 4. Redeem API (Static for now)
 app.get('/redeem', (req, res) => {
-    res.send("<h1>ReneFN Code Redemption</h1><p>Redeem system is currently under maintenance.</p>");
+    res.send("<h1>Redeem Page</h1><p>Redeem system coming soon.</p>");
 });
 
 app.listen(PORT, () => {
